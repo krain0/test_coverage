6e6c37651a594feb55dca2d963f674f93ed1eb76
Daniel <danielogen@gmail.com>
2024-08-15 14:23:12 -0700

    feat: update pytesting

--- tests/test_account.py
+++ tests/test_account.py
@@ -3,52 +3,48 @@ Test Cases TestAccountModel
 """
 import json
 from random import randrange
-from unittest import TestCase
+import pytest
 from models import db, app
 from models.account import Account, DataValidationError
 
 ACCOUNT_DATA = {}
 
-class TestAccountModel(TestCase):
-    """Test Account Model"""
+@pytest.fixture(scope="module", autouse=True)
+def load_account_data():
+    """ Load data needed by tests """
+    global ACCOUNT_DATA
+    with open('tests/fixtures/account_data.json') as json_data:
+        ACCOUNT_DATA = json.load(json_data)
 
-    @classmethod
-    def setUpClass(cls):
-        """ Load data needed by tests """
-        db.create_all()  # make our sqlalchemy tables
-        global ACCOUNT_DATA
-        with open('tests/fixtures/account_data.json') as json_data:
-            ACCOUNT_DATA = json.load(json_data)
+    # Set up the database tables
+    db.create_all()
+    yield
+    db.session.close()
 
-    @classmethod
-    def tearDownClass(cls):
-        """Disconnext from database"""
-        db.session.close()
+@pytest.fixture(scope="function", autouse=True)
+def setup_and_teardown():
+    """ Truncate the tables and set up for each test """
+    db.session.query(Account).delete()
+    db.session.commit()
+    yield
+    db.session.remove()
 
-    def setUp(self):
-        """Truncate the tables"""
-        self.rand = randrange(0, len(ACCOUNT_DATA))
-        db.session.query(Account).delete()
-        db.session.commit()
+######################################################################
+#  T E S T   C A S E S
+######################################################################
 
-    def tearDown(self):
-        """Remove the session"""
-        db.session.remove()
-
-    ######################################################################
-    #  T E S T   C A S E S
-    ######################################################################
-
-    def test_create_all_accounts(self):
-        """ Test creating multiple Accounts """
-        for data in ACCOUNT_DATA:
-            account = Account(**data)
-            account.create()
-        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))
-
-    def test_create_an_account(self):
-        """ Test Account creation using known data """
-        data = ACCOUNT_DATA[self.rand] # get a random account
+def test_create_all_accounts():
+    """ Test creating multiple Accounts """
+    for data in ACCOUNT_DATA:
         account = Account(**data)
         account.create()
-        self.assertEqual(len(Account.all()), 1)
+    assert len(Account.all()) == len(ACCOUNT_DATA)
+
+def test_create_an_account():
+    """ Test Account creation using known data """
+    rand = randrange(0, len(ACCOUNT_DATA))
+    data = ACCOUNT_DATA[rand]  # get a random account
+    account = Account(**data)
+    account.create()
+    assert len(Account.all()) == 1
+
