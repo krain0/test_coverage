5322a0d5a9fba1211ac126530eaf51ac2ef38771
Kyle Rainey <rainek1@unlv.nevada.edu>
2024-09-16 03:41:58 -0700

    Added tests to achieve 100% code coverage
    Tests added: test_repr, test_to_dict, test_from_dict, test_update_withID, test_update_noID, test_delete

--- tests/test_account.py
+++ tests/test_account.py
@@ -48,3 +48,89 @@ def test_create_an_account():
     account.create()
     assert len(Account.all()) == 1
 
+
+# ADDED
+def test_repr():
+    """Test the representation of an account"""
+    account = Account()
+    account.name = "Foo"
+    assert str(account) == "<Account 'Foo'>"
+
+def test_to_dict():
+    """ Test account to dict """
+    rand = randrange(0, len(ACCOUNT_DATA))  # Generate a random index
+    data = ACCOUNT_DATA[rand]  # get a random account
+    account = Account(**data)
+    result = account.to_dict()
+
+    assert account.name == result["name"]
+    assert account.email == result["email"]
+    assert account.phone_number == result["phone_number"]
+    assert account.disabled == result["disabled"]
+    assert account.date_joined == result["date_joined"]
+
+def test_from_dict():
+    result = dict();
+    result["name"] = "John"
+    result["email"] = "john@mail.com"
+    result["phone_number"] = "1234567"
+
+    account = Account()
+    account.from_dict(result)
+
+    assert account.name == result["name"]
+    assert account.email == result["email"]
+    assert account.phone_number == result["phone_number"]
+    # assert account.disabled == result["disabled"]
+    # assert account.date_joined == result["date_joined"]
+
+
+def test_update_withID():
+    # creating example info, with an ID
+    newAccountInfo = dict()
+    newAccountInfo["name"] = "Sam Sam"
+    newAccountInfo["email"] = "sam@sam.sam"
+    newAccountInfo["phone_number"] = "123.456.789"
+    newAccountInfo["disabled"] = False
+    newAccountInfo["id"] = 123
+
+    account = Account(**newAccountInfo)
+    userId = account.id
+    account.create()
+    
+    newEmail = "sam_" + account.email
+
+    account.email = newEmail
+    account.update()
+    
+    newAccount = Account.find(userId)
+    assert newAccount.email == newEmail
+
+
+def test_update_noID():
+    rand = randrange(0, len(ACCOUNT_DATA))  # Generate a random index
+    data = ACCOUNT_DATA[rand]  # get a random account
+    account = Account(**data)
+    try:
+        # example data has no ID, so this should throw an exception
+        account.update()
+        assert False
+    except DataValidationError:
+        assert True
+
+def test_delete():
+    # creating example info, with an ID
+    newAccountInfo = dict()
+    newAccountInfo["name"] = "Sam Sam"
+    newAccountInfo["email"] = "sam@sam.sam"
+    newAccountInfo["phone_number"] = "123.456.789"
+    newAccountInfo["disabled"] = False
+    newAccountInfo["id"] = 123
+
+    account = Account(**newAccountInfo)
+    userId = account.id
+    account.create()
+    assert Account.find(userId) != None # make sure it was created
+    account.delete()
+    assert Account.find(userId) == None # make sure it is gone
+    
